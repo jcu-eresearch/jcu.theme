bb6b4e757a04c51513e103ae11a850942b906ab8
David Breitkreutz <david.breitkreutz@jcu.edu.au>
2009-05-07 09:22:26 +1000

    Added uninstall profile and corrected skins generic setup

---
+++ net/theme/Extensions/Install.py
@@ -0,0 +1,11 @@
+from Products.CMFCore.utils import getToolByName
+
+def install(portal):
+    setup_tool = getToolByName(portal, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile('profile-net.theme:default')
+    return "Ran all import steps."
+
+def uninstall(portal):
+    setup_tool = getToolByName(portal, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile('profile-net.theme:uninstall')
+    return "Ran all uninstall steps."
