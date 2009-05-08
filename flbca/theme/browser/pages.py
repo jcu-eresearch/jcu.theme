9b03365502da134ffdafd1eaf66239df7bbd87b5
David Breitkreutz <david.breitkreutz@jcu.edu.au>
2009-05-08 12:28:21 +1000

    Updated css and the calendar view for the collage homepage

--- flbca/theme/browser/pages.py
+++ flbca/theme/browser/pages.py
@@ -6,9 +6,12 @@ from Products.Collage.browser.views import BaseView
 from dateable.chronos.browser.month import MonthView
 from dateable.chronos.browser.base_view import DAYS, MONTHS
 import datetime
+import textwrap
 
 class CollageDateableMonthPage(BaseView, MonthView):
      title = u'p4a-calendar'
+     max_length = 18
+     max_line_length = 10
 
      #Need to call the parent classes init methods as they don't
      #get done automatically in Python (at least not yet)
@@ -22,4 +25,19 @@ class CollageDateableMonthPage(BaseView, MonthView):
          self._default_day = datetime.date.today()
          MonthView.calcInfo(self)
          self._default_day = currentDefaultDay
+
+     def eventTextDisplay(self, eventText):
+         wrappableTitle = eventText[:self.max_length]
+
+         wrapper = textwrap.TextWrapper(width=self.max_line_length)
+         wrapper.break_on_hyphens = False
+         wrappableTitle = '<br />'.join(wrapper.wrap(wrappableTitle))
+
+         if len(eventText) > self.max_length:
+             wrappableTitle += '...'
+         
+         return wrappableTitle
+
+     def countMoreEvents(self, day):
+        return (len(self.getOccurrencesInDay(day)) - (self.calendar_tool.displaymaxevents-1) )
 
