10922518c64897d69168be0af9da015a852acc40
David Breitkreutz <david.breitkreutz@jcu.edu.au>
2009-08-05 15:30:49 +1000

    Added security relaxations for the Google Calendar function

--- jcu/theme/__init__.py
+++ jcu/theme/__init__.py
@@ -1,3 +1,7 @@
+from AccessControl import ModuleSecurityInfo
+
+ModuleSecurityInfo('jcu.theme.Extensions.GoogleCalendar').declarePublic('createGoogleCalendarEvent')
+
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
