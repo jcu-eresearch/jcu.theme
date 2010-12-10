f43e99fd5a177c826134d0937a54b88db46a8ec8
David Breitkreutz <david.breitkreutz@jcu.edu.au>
2010-12-10 15:53:58 +1000

    Adding LDAP GS profile (and hacks) and removing the old prefs groups overview

--- jcu/theme/setuphandlers.py
+++ jcu/theme/setuphandlers.py
@@ -1,3 +1,6 @@
+from zope.app.component.hooks import getSite
+
+
 def setupVarious(context):
 
     # Ordinarily, GenericSetup handlers check for the existence of XML files.
@@ -9,12 +12,34 @@ def setupVarious(context):
         return
 
     # Add additional setup code here
+    site = getSite()
+
+    #Set up our reCAPTCHA keys for forms, etc
     try:
         from collective.recaptcha.settings import getRecaptchaSettings
         settings = getRecaptchaSettings()
         settings.public_key = u'6Ld5c78SAAAAAJH6NdN69ZYiXGrYcs9MLrvjY08U'
         settings.private_key = u'6Ld5c78SAAAAAGJpMrcbF_vAzeVRNWCjIWFZPZfs'
     except:
+        site.plone_log('Could not configure reCAPTCHA keys.')
+        pass
+
+    #Customise the parts of the LDAP plugin our GS doesn't touch yet.
+    try:
+        acl_users = site.acl_users
+        plugins = acl_users.plugins
+        plugins.deactivatePlugin(
+            plugins._getInterfaceFromName('IUserManagement'),
+            'ldap-plugin',
+        )
+        ldap_plugin = acl_users['ldap-plugin']
+        ldap_acl_users = ldap_plugin.acl_users
+        ldap_acl_users.read_only = True
+        ldap_acl_users._pwd_encryption = 'SSHA'
+        ldap_acl_users._roles = ['Anonymous']
+        import ipdb; ipdb.set_trace()
+    except:
+        site.plone_log('Could not configure LDAP plugin settings.')
         pass
 
 
