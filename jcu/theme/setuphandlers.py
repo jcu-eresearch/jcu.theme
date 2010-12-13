79fa95369e3590970a35ad5170cbead076d4a994
David Breitkreutz <david.breitkreutz@jcu.edu.au>
2010-12-13 16:21:20 +1000

    Making JCU product depend on plone.app.caching, and fixing issue with TinyMCE editor

--- jcu/theme/setuphandlers.py
+++ jcu/theme/setuphandlers.py
@@ -14,12 +14,27 @@ def setupVarious(context):
     # Add additional setup code here
     site = getSite()
 
+    #Set up the new Plone 4 MailHost for queuing
+    try:
+        site.MailHost.manage_makeChanges(
+            title='Mail server settings for outgoing mail',
+            smtp_host='smtp.jcu.edu.au',
+            smtp_port='25',
+            smtp_queue=True,
+            smtp_queue_directory='/tmp/mailhost'
+        )
+        site.plone_log('MailHost configured for queuing.')
+    except:
+        site.plone_log('Could not reconfigure MailHost for queuing.')
+        pass
+
     #Set up our reCAPTCHA keys for forms, etc
     try:
         from collective.recaptcha.settings import getRecaptchaSettings
         settings = getRecaptchaSettings()
         settings.public_key = u'6Ld5c78SAAAAAJH6NdN69ZYiXGrYcs9MLrvjY08U'
         settings.private_key = u'6Ld5c78SAAAAAGJpMrcbF_vAzeVRNWCjIWFZPZfs'
+        site.plone_log('reCAPTCHA keys configured.')
     except:
         site.plone_log('Could not configure reCAPTCHA keys.')
         pass
@@ -37,9 +52,30 @@ def setupVarious(context):
         ldap_acl_users.read_only = True
         ldap_acl_users._pwd_encryption = 'SSHA'
         ldap_acl_users._roles = ['Anonymous']
+        site.plone_log('LDAP plugin settings configured.')
     except:
         site.plone_log('Could not configure LDAP plugin settings.')
         pass
+
+    #We can import plone.app.caching profiles (we have already via
+    #metadata.xml) but we need to manually turn it on.
+    try:
+        from zope.component import getUtility
+        from plone.registry.interfaces import IRegistry
+        from plone.caching.interfaces import ICacheSettings
+        from plone.app.caching.interfaces import IPloneCacheSettings
+
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(ICacheSettings)
+        settings.enabled = True
+        ploneSettings = registry.forInterface(IPloneCacheSettings)
+        ploneSettings.enableCompression = True
+        site.plone_log('plone.app.caching settings configured.  Caching and \
+                       compression are now active.')
+    except:
+        site.plone_log('Could not configure plone.app.caching settings.')
+
+
 
 
 
