1121d3e14a4f2d327aff3a1e4c8ccb5e4bc7c2b4
David\ Beitey <david@davidjb.com>
2012-05-09 16:23:41 +1000

    Monkey patch Products.CAS4PAS

--- jcu/theme/__init__.py
+++ jcu/theme/__init__.py
@@ -5,6 +5,10 @@ from AccessControl import ModuleSecurityInfo
 module = ModuleSecurityInfo('jcu.theme.Extensions.GoogleCalendar') \
             .declarePublic('createGoogleCalendarEvent')
 
+#XXX Monkey patch until CAS4PAS is fixed
+import Products.CAS4PAS.CASAuthHelper
+from zLOG import DEBUG
+Products.CAS4PAS.CASAuthHelper.ERROR = DEBUG
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
